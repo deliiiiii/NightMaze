a00704e9ebc6af81246f2c3c5b97a5df5b38eb9f
Deli <1765939870@qq.com>
2026-03-23 01:13:28 +0800

    feat：源生static abstract；并实现上下文补全（真好玩）

--- Assets/Scripts/NM/View/ZZZTest/TestSetSpeed.cs
+++ Assets/Scripts/NM/View/ZZZTest/TestSetSpeed.cs
@@ -1,4 +1,8 @@
-﻿using DG.Tweening;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DG.Tweening;
+using General;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -12,23 +16,52 @@ internal class TestSetSpeed : MonoBehaviour
         DOTween.timeScale = tar;
     }
 
-    public void Test<T>() where T : ITestInterface<T>
+    [Button]
+    public static void CallTest() => Test<TestIns>();
+    [Button]
+    public static void CallTest3()
+    {
+        List<TestIns> l = [new (){Int = 1}, new (){Int = 2}, new (){Int = 3}];
+        Test3(l);
+    }
+
+    static void Test<T>() where T : ITestInterface<T>
     {
         var x = Factory<T>.Create();
+        var x2 = Factory<T>.Create2();
+        MyDebug.Log(x2);
+    }
+
+    static void Test3<T>(List<T> list) where T : ITestInterface<T>
+    {
+        var x3 = Factory<T>.Create3(list);
+        MyDebug.Log(x3);
     }
 }
 
-public static class TExt
-{
-    extension<T>(Factory<T>) where T : ITestInterface<T>
-    {
-        public static T Create() => default!;
-    }
-}
+[AttributeUsage(AttributeTargets.Method)]
+public class StaticAbstractAttribute : Attribute;
 // ReSharper disable once UnusedTypeParameter
 public static class Factory<T>;
 public interface ITestInterface<out T>
 {
-    // CS9281: 目标运行时不支持接口中的 static abstract 成员
-    // static abstract T Create();
+    [StaticAbstract] static T Create() => default!;
+    [StaticAbstract] static int Create2() => 0;
+    [StaticAbstract] static T Create3(List<T> list) => default!;
 }
+
+public class TestIns : ITestInterface<TestIns>
+{
+    public int Int;
+    public override string ToString()
+    {
+        return $"TestIns {{ Int = {Int} }}";
+    }
+    public static TestIns Create() => new();
+    public static int Create2() => 42;
+    public static TestIns Create3(List<TestIns> list) 
+        => new() { Int = list.Sum(x => x.Int) };
+}
+
+public interface I2<T>;
+public class I2Ins<T> : I2<T>;
